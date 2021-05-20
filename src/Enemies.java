9a177937faf7f9e02bde9a301267988832e80847
faustinaryu <faustinaryu@localhost>
2021-05-20 08:48:59 -0700

    paints enemies

--- src/Enemies.java
+++ src/Enemies.java
@@ -5,12 +5,12 @@ public class Enemies extends Character {
 	int y;
 	int width; 
 	int height;
-	
-	//hello
-			
+				
 	// default constructor, sets all to zero
 	public Enemies(String filename, int w, int h) {
 			super(filename,w,h);
+			x+=100;
+			y=20;
 	}
 		
 	public String toString() {
