fa9a9f9acbd6b736bd9170a40702eff8ab189c27
AngelaYu-3 <yuangela12@gmail.com>
2021-05-24 09:25:21 -0700

    added pea class

--- src/Pea.java
+++ src/Pea.java
@@ -1,4 +1,9 @@
 
-public class Pea {
+public class Pea extends Character{
+
+	public Pea(String fileName, int width, int height, int x, int y, int vx, int vy) {
+		super(fileName, width, height, x, y, vx, vy);
+		// TODO Auto-generated constructor stub
+	}
 
 }
